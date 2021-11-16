1898b27677310a36167171603abbe1f8fba25466
Roman Kolivashko <rkolivashko@gmail.com>
2021-11-16 15:54:25 -0800

    Added working listing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import React from "react";
 import Header from "./components/Header";
 import Days from "./components/Days"
-//import Location from "./components/Location";
+import Location from "./components/Location";
 import LocationControl from "./components/LocationControl";
 
 function App() {
@@ -27,12 +27,14 @@ function App() {
           <div className="row">
             <div className="col">
               <div className="box"><LocationControl />
+              {/* <div className="box"><Location /> */}
               </div>
             </div>
           </div>
           <div className="row">
             <div className="col">
-              <div className="box">Footer
+              <div className="box"><Location />
+              {/* <div className="box">Footer */}
               </div>
             </div>
           </div>
